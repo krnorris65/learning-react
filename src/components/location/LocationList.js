3b68dd298eb15c73746ad0be65d9569d0f2bca40
Kristen Norris <krnorris65@gmail.com>
2019-03-11 15:43:29 -0500

    completed Kennel practice exercise

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component {
+    render() {
+        return (
+            <article>
+            <section>
+                <h4>Nashville North Location</h4>
+                <h5>500 Puppy Way</h5>
+            </section>
+            <section>
+                <h4>Nashville South Location</h4>
+                <h5>860 Terrier Park</h5>
+            </section>
+            </article>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-
+import Kennel from "./components/Kennel"
 
 ReactDOM.render(<Kennel />, document.querySelector("#root"));
