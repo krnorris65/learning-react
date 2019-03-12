5e1b30d8ccba2cd4a7c5cdff6fb648204b3bc222
Kristen Norris <krnorris65@gmail.com>
2019-03-12 10:28:11 -0500

    completed chap 2 challenge

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -3,28 +3,35 @@ import React, { Component } from 'react'
 
 class AnimalList extends Component {
     render() {
+        //array of animalOwners
+        let animalOwner = this.props.animalOwners
+        //array of owners
+        let ownerList = this.props.owners
         return (
             <section className="animals">
                 {
-                    this.props.animals.map(animal =>
+                    this.props.animals.map(animal => {
+                        //finding animalOwner relationship for current Animal
+                        let relationship = animalOwner.filter(animalRel => animalRel.animalId === animal.id)
 
-                        <div key={animal.id}>
-                            {animal.name} ({animal.type})
+                        //array of owners for current animal
+                        let ownerArray = relationship.map(ownerRel => {
+                            //getting name of the owner for the current animalOwner relationship. 
+                            let curOwner = ownerList.filter(owner => {
+                                if(owner.id === ownerRel.ownerId){
+                                    return owner
+                                }
+                            })
+                            // curOwner is an array of one object
+                            return curOwner[0]
+                        })
+
+                        return <div key={animal.id}>
+                            <h1>{animal.name} ({animal.type})</h1>
+                            {ownerArray.map(o => <p>{o.name}</p>)}
                         </div>
+                    }
                     )
-                }
-                {
-                    this.props.owners.map(owner =>
-                        <div key={owner.id}>
-                            {owner.name}
-                        </div>)
-                }
-                {
-                    this.props.animalOwners.map(animalOwner =>
-                        <div key={animalOwner.id}>
-                            Animal{animalOwner.animalId}
-                            Owner{animalOwner.ownerId}
-                        </div>)
                 }
             </section>
         );
