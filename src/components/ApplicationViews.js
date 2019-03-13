d4c1ee320173acd9eb81c727e34f44a1f6d9a2b9
Kristen Norris <krnorris65@gmail.com>
2019-03-13 16:10:40 -0500

    completed chapter 4 of react

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,52 +6,35 @@ import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnerList'
 
 
-class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
+export default class ApplicationViews extends Component {
 
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    ownersFromAPI = [
-        { id: 1, name: "Ryan Tanay", phone: "293-304-3029" },
-        { id: 2, name: "Emma Beaton", phone: "283-294-2943" },
-        { id: 3, name: "Dani Adkins", phone: "748-398-9483" },
-        { id: 4, name: "Adam Oswalt", phone: "829-234-3492" },
-        { id: 5, name: "Fletcher Bangs", phone: "283-234-5434" },
-        { id: 6, name: "Angela Lee", phone: "984-394-9238" }
-    ]
-    
-    animalOwnersFromAPI = [
-        {id: 1, animalId: 1, ownerId: 6},
-        {id: 2, animalId: 2, ownerId: 4},
-        {id: 3, animalId: 3, ownerId: 3},
-        {id: 4, animalId: 3, ownerId: 5},
-        {id: 5, animalId: 4, ownerId: 1},
-        {id: 6, animalId: 4, ownerId: 2}
-    ]
     
     state = {
-        owners: this.ownersFromAPI,
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        animalOwners: this.animalOwnersFromAPI
+        owners: [],
+        employees: [],
+        locations: [],
+        animals: [],
+        animalOwners: []
+    }
+
+    componentDidMount() {
+        const newState = {}
+
+        fetch("http://localhost:5002/owners")
+            .then(r => r.json())
+            .then(owners => newState.owners = owners)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => fetch("http://localhost:5002/locations")
+            .then(r => r.json()))
+            .then(locations => newState.locations = locations)
+            .then(() => fetch("http://localhost:5002/animals")
+            .then(r => r.json()))
+            .then(animals => newState.animals = animals)
+            .then(() => fetch("http://localhost:5002/animalOwners")
+            .then(r => r.json()))
+            .then(animalOwners => newState.animalOwners = animalOwners).then(()=> this.setState(newState))
     }
 
     render() {
@@ -60,18 +43,16 @@ class ApplicationViews extends Component {
                 <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
                 }} />
-                <Route path="/animals" render={(props) => {
+                <Route exact path="/animals" render={(props) => {
                     return <AnimalList animals={this.state.animals} owners={this.state.owners} animalOwners={this.state.animalOwners}/>
                 }} />
-                <Route path="/employees" render={(props) => {
+                <Route exact path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
-                <Route path="/owners" render={(props) => {
+                <Route exact path="/owners" render={(props) => {
                     return <OwnerList owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
     }
 }
-
-export default ApplicationViews
