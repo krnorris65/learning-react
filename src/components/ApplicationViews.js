a3ea36b601c38b2c82cdbc6586c8db6bb7366e11
Kristen Norris <krnorris65@gmail.com>
2019-04-04 14:44:14 -0500

    added list of employees under the locations

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -119,7 +119,7 @@ export default class ApplicationViews extends Component {
                 <Route path="/login" component={Login}/>
                 <Route exact path="/" render={(props) => {
                     if(this.isAuthenticated()){
-                        return <LocationList locations={this.state.locations} />
+                        return <LocationList locations={this.state.locations} employees={this.state.employees}/>
                     } else {
                         return <Redirect to="/login"/>
                     }
--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -8,6 +8,16 @@ export default class LocationList extends Component {
                         <section key={location.id}>
                             <h4>{location.name}</h4>
                             <h5>{location.address}</h5>
+                            <div className="location-employees">
+                            <h6>Employees:</h6>
+                            <ul>
+                            {
+                                this.props.employees
+                                .filter(emp => emp.locationId === location.id)
+                                .map(emp => <li>{emp.name}</li>)
+                            }
+                            </ul>
+                            </div>
                         </section>)
                 }
 
